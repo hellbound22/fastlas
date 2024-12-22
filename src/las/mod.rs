b725328f10b2363eeb20a82dbc44ee0cd2286dd2
Rodrigo Mauricio <rodrigomauricio@protonmail.com>
2024-12-22 20:58:53 -0300

    CLI implementation

--- src/las/mod.rs
+++ src/las/mod.rs
@@ -1,9 +1,7 @@
 use std::fs::File;
 use std::io::BufReader;
 use std::io::BufWriter;
-use std::io::Read;
 use std::io::Write;
-
 
 use memmap2::Mmap;
 
@@ -16,29 +14,33 @@ pub mod points;
 
 #[derive(Debug)]
 pub struct LasFile {
-    header: header::PublicHeaderBlock,
-    pub cloud: points::PointCloud,
+    file_desc: File,
+    pub header: header::PublicHeaderBlock,
+    pub cloud: Option<points::PointCloud>,
 }
 
 impl LasFile {
-    pub fn new_from_file(file: File) -> Self {
-        let mmap = unsafe { Mmap::map(&file).unwrap()  };
-
-        let mut reader = BufReader::new(file);
+    pub fn new_from_file(file_desc: File) -> Self {
+        let mut reader = BufReader::new(&file_desc);
         let raw_head = PublicHeaderBlockRaw::new_from_reader(&mut reader);
         let header = PublicHeaderBlock::new_from_raw(&raw_head);
         
-        //let cloud = PointCloud::parse_number(&mmap, &header, 3);
-        let cloud = PointCloud::parse_all(&mmap, &header);
+        Self { header, file_desc, cloud: None }
+    }
 
+    pub fn read_point_cloud(&mut self) {
+        let mmap = unsafe { Mmap::map(&self.file_desc).unwrap()  };
+        let cloud = PointCloud::parse_all(&mmap, &self.header);
 
-        Self { header, cloud }
+        self.cloud = Some(cloud);
     }
 
     pub fn write_points_to_file(&self, file: &mut File) {
-        let mut buf = BufWriter::new(file);
-        for point in &self.cloud.v {
-            buf.write(point.format_to_txt().as_bytes()).unwrap();
+        if let Some(pc) = &self.cloud {
+            let mut buf = BufWriter::new(file);
+            for point in &pc.v {
+                buf.write(point.format_to_txt().as_bytes()).unwrap();
+            }
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
+use clap::{Parser, Subcommand};
+
 use std::{fs::File, path::{Path, PathBuf}};
-use clap::Parser;
 
 mod las;
 mod utils;
@@ -7,10 +8,18 @@ mod utils;
 use las::{header::{PublicHeaderBlock, PublicHeaderBlockRaw}, LasFile};
 
 
+#[derive(Subcommand, Clone)]
+enum Command {
+    Header,
+}
+
 #[derive(Parser)]
 struct Cli {
     input_file: Option<String>,
     output_file: Option<String>,
+
+    #[command(subcommand)]
+    command: Option<Command>,
 }
 
 fn main() -> std::io::Result<()> {
@@ -24,19 +33,30 @@ fn main() -> std::io::Result<()> {
 
     let in_file = File::open(&input)?;
 
-    let output = if let Some(out) = cli.output_file{
-        PathBuf::from(&out)
-    } else {
-        let p = PathBuf::from(&format!("{}.txt", input.file_stem().unwrap().to_str().unwrap()));
-        println!("Output file not specified. Using {}", p.display());
-        p
-    };
+    let mut las = LasFile::new_from_file(in_file);
 
-    let mut out_file = File::create(&output)?;
+    match cli.command {
+        Some(Command::Header) => {
+            dbg!(las.header);
+        },
+        None => {
+            let output = if let Some(out) = cli.output_file{
+                PathBuf::from(&out)
+            } else {
+                let p = PathBuf::from(&format!("{}.txt", input.file_stem().unwrap().to_str().unwrap()));
+                println!("Output file not specified. Using {}", p.display());
+                p
+            };
 
-    let las = LasFile::new_from_file(in_file);
+            las.read_point_cloud();
 
-    las.write_points_to_file(&mut out_file);
+            let mut out_file = File::create(&output)?;
+            las.write_points_to_file(&mut out_file);
+        }
+    }
+
+
+
     Ok(())
 }
 
